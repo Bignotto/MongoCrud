5e1842e03720ca986df768dbb77f26351ad99870
Thiago Bignotto <bignotto@gmail.com>
2019-03-22 04:09:49 +0000

    show all users registered

--- server.js
+++ server.js
@@ -10,7 +10,7 @@ app.set('view engine','ejs')
 
 MongoClient.connect(_config.dbUrl, (err, client) => {
     if(err) return console.log(err)
-    db = client.db('authteste')
+    db = client.db('authtest')
 
     app.listen(3000,function(){
         console.log('Server is UP on port 3000.')
@@ -20,12 +20,26 @@ MongoClient.connect(_config.dbUrl, (err, client) => {
 app.get('/', (req,res) => {
     res.render('index.ejs')
 })
+app.get('/', (req, res) => {
+    var cursor = db.collection('users').find()
+})
 
 app.post('/show',(req,res) => {
-    db.collection('data').save(req.body, (err,result) => {
+    db.collection('users').insertOne(req.body, (err,result) => {
         if(err) return console.log(err)
 
         console.log('saved into database!')
         res.redirect('/')
+        db.collection('users').find().toArray((err,results) => {
+            console.log(results)
+        })
+    })
+})
+
+app.get('/show', (req, res) => {
+    db.collection('users').find().toArray((err, results) => {
+        if (err) return console.log(err)
+        res.render('show.ejs', { data: results })
+
     })
 })
