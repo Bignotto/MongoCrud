ac256f8772877aafc1d0d03df682af819a419fbc
Thiago Bignotto <bignotto@gmail.com>
2019-03-27 03:44:30 +0000

    bug fixes and code improvements

--- server.js
+++ server.js
@@ -3,11 +3,15 @@ const bodyParser = require('body-parser')
 const _config = require('./config')
 
 const MongoClient = require('mongodb').MongoClient;
-var ObjectId = require('mongodb').ObjectID;
+
+const user = require('./routes/user')
+
 const app = express()
 
 app.use(bodyParser.urlencoded({ extended : true}))
 app.set('view engine','ejs')
+
+app.use('/user',user)
 
 MongoClient.connect(_config.dbUrl, { useNewUrlParser: true } ,(err, client) => {
     if(err) return console.log(err)
@@ -21,59 +25,7 @@ MongoClient.connect(_config.dbUrl, { useNewUrlParser: true } ,(err, client) => {
 app.get('/', (req,res) => {
     res.render('index.ejs')
 })
-app.get('/', (req, res) => {
-    var cursor = db.collection('users').find()
-})
+// app.get('/', (req, res) => {
+//     var cursor = db.collection('users').find()
+// })
 
-app.post('/show',(req,res) => {
-    db.collection('users').insertOne(req.body, (err,result) => {
-        if(err) return console.log(err)
-
-        console.log('saved into database!')
-        res.redirect('/')
-        db.collection('users').find().toArray((err,results) => {
-            console.log(results)
-        })
-    })
-})
-
-app.get('/show', (req, res) => {
-    db.collection('users').find().toArray((err, results) => {
-        if (err) return console.log(err)
-        res.render('show.ejs', { data: results })
-
-    })
-})
-
-app.route('/edit/:id')
-    .get((req,res) => {
-        var id = req.params.id
-        db.collection('users').find(ObjectId(id)).toArray((err,result) => {
-            if(err) return res.send(err)
-            res.render('edit.ejs', {data: result})
-        })
-    })
-    .post((req,res) => {
-        var id = req.params.id
-        var name = req.body.name
-        var surname = req.body.surname
-        db.collection('users').updateOne({_id: ObjectId(id)}, {
-            $set : {
-                name: name,
-                surname: surname
-            }
-        },(err,result) => {
-            if(err) return res.send(err)
-            res.redirect('/show')
-            console.log('BD updated!!')
-        })
-    })
-
-app.route('/delete/:id')
-    .get((req,res) => {
-        var id = req.params.id
-        db.collection('users').deleteOne({_id: ObjectId(id)}, (err,result) => {
-            console.log('Deleted... :(')
-            res.redirect('/show')
-        })
-    })
